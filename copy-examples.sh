edee9185b13576b12bbdad3df89a12b7bc7f8ebe
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2024-08-21 00:33:43 +0600

    Update copy-examples.sh to make work in MacOS as well

--- copy-examples.sh
+++ copy-examples.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Copy docker compose example files
-cp .env.example .env
-cp docker-compose.override.example.yml docker-compose.override.yml
+cp -n .env.example .env
+cp -n docker-compose.override.example.yml docker-compose.override.yml
 
 # Create {name}.env files from .example.env files in .envs directory
-find .envs/ -type f | grep -Po '([a-z\-]+)(?=.example.env)' | xargs -i cp .envs/{}.example.env .envs/{}.env
+find .envs -type f | grep -E '[a-z-]+\.example\.env$' | cut -d'.' -f1 | while read prefix; do cp -n "${prefix}.example.env" "${prefix}.env"; done
