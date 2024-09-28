df4263a7286b0a4a3d3c0bec9036b511f89555d1
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2024-09-28 22:05:41 +0600

    Update copy-examples.sh for Linux & MacOS

--- copy-examples.sh
+++ copy-examples.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Copy docker compose example files
-cp -n .env.example .env
-cp -n docker-compose.override.example.yml docker-compose.override.yml
+cp --update=none .env.example .env
+cp --update=none docker-compose.override.example.yml docker-compose.override.yml
 
 # Create {name}.env files from .example.env files in .envs directory
-find .envs -type f | grep -E '[a-z-]+\.example\.env$' | cut -d'.' -f1 | while read prefix; do cp -n "${prefix}.example.env" "${prefix}.env"; done
+if [[ -f /etc/issue ]]; then
+  # If the file exists, it's likely a Linux system
+  find .envs/ -type f | grep -Po '([a-z\-]+)(?=.example.env)' | xargs -i cp --update=none .envs/{}.example.env .envs/{}.env
+elif [[ -f /System/Library/CoreServices/SystemVersion.plist ]]; then
+  # If the macOS-specific SystemVersion.plist file exists, it's likely a macOS system
+  find .envs -type f | grep -E '[a-z-]+\.example\.env$' | cut -d'.' -f1 | while read prefix; do cp --update=none "${prefix}.example.env" "${prefix}.env"; done
+fi
